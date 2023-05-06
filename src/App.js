647a4ed01a96bc0edddd83f476547fa7b1eabf12
Shulaev Ivan <mail@ntdservice.ru>
2023-05-06 17:34:54 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import "./components/Card/Card.css";
 function App() {
   return (
     <div className="App">
-    <Card></Card>
+    <Card 
+      title = {'Card Title'}
+      text = {'Some quick example text to build on the card title and make up the bulk of the card s content.'}
+    >
+      <img src="https://via.placeholder.com/300x150" alt="alt-test" />
+    </Card>
+
+    <Card 
+      title = {'Card Title'}
+      text = {'With supporting text below as a natural lead-in to additional content.'}
+    >
+
+    </Card>
     </div>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
+    console.log(props.children)
     return (
         <React.Fragment>
             <div className="card-wrap">
                 <div className="image-wrap">
-                    <p className="image-text">image cap</p>
-                    <img src="https://via.placeholder.com/300x150" />
+                    {props.children && props.children.type === 'img' ?  <p className="image-text">{props.children.props.alt}</p> : null}
+                    {props.children}
                 </div>
                 <div className="card-body">
                     <div className="card-title-wrap">
-                        <h3>Card-Title</h3>
+                        <h3>{props.title}</h3>
                     </div>
                     <div className="card-content">
-                       <p>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                       <p>{props.text}</p>
                     </div>
                     <div className="card-btn-wrap">
                         <button className="card-btn">Go somwhere</button>
@@ -23,7 +24,7 @@ const Card = () => {
             </div>
             
         </React.Fragment>
-    )
+    );
 }
 
 export default Card;
